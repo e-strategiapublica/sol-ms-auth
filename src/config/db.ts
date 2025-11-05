5dff4dc9a2711fd31581dc556ac6ef4d0ced6f35
Lucassum <lucas.tavares@e-strategiapublica.com.br>
2025-11-05 19:53:30 -0300

    feat: configure environment variables for database connection and server setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,11 @@ import { Kysely, PostgresDialect } from "kysely";
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    database: "sol_ms_auth",
-    host: "localhost",
-    user: "changeme",
-    password: "changeme",
-    port: 5432,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     max: 10,
   }),
 });
